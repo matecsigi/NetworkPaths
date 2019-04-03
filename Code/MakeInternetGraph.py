f830f4ba603d54e04495f1d9efb4b6d2f9a978fb
EC2 Default User <ec2-user@ip-172-31-24-185.ec2.internal>
2019-04-03 07:31:44 +0000

    Create the internet graph from multiple files

--- Code/MakeInternetGraph.py
+++ Code/MakeInternetGraph.py
@@ -1,15 +1,33 @@
 import networkx as nx
 import igraph
+import glob
+import datetime
+
+def graphFromFile(fileName):
+    G = nx.DiGraph()
+    with open(fileName, 'r') as f:
+        for line in f:
+            splitList = line.split()
+            if splitList[0] == 'D':
+                G.add_edge(splitList[1], splitList[2])
+    return G
+
+folderName = '../Data/AsLinks/'
+files = glob.glob(str(folderName)+'*.txt')
 
 G = nx.DiGraph()
 
-fileName = 'cycle-aslinks.l7.t1.c006274.20180101.txt'
-with open('../Data/'+fileName, 'r') as f:
-    for line in f:
-        splitList = line.split()
-        if splitList[0] == 'D':
-            G.add_edge(splitList[1], splitList[2])
+for f in files:
+    fileGraph = graphFromFile(f)
+    G.add_edges_from(fileGraph.edges())
+    print(len(G.nodes))
 
-print len(G.nodes())
+nx.write_gml(G, 'as-graph-'+str(datetime.datetime.today().strftime('%Y-%m-%d'))+".gml")
 
-nx.write_gml(G, 'graph-'+fileName[:-4]+".gml")
+# Basic graph merging
+# fileName1 = 'cycle-aslinks.l7.t1.c006274.20180101.txt'
+# fileName2 = 'cycle-aslinks.l7.t1.c006277.20180102.txt'
+# G1 = graphFromFile(fileName1)
+# G2 = graphFromFile(fileName2)
+# G = G1.copy()
+# G.add_edges_from(G2.edges())
