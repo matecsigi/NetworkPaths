c874d7b35d47b260d6532cce2578be79b9e61317
EC2 Default User <ec2-user@ip-172-31-24-185.ec2.internal>
2019-04-25 09:31:49 +0000

    Plotting the weighted path graph with networkx

--- Code/WeightedPathsFromNode.py
+++ Code/WeightedPathsFromNode.py
@@ -1,8 +1,19 @@
 import networkx as nx
 import igraph
 import datetime
+import matplotlib.pyplot as plt
+import math
+import random
+
+def generatePosition(distance):
+    angle = random.uniform(0, 2*math.pi)
+    x = distance*math.cos(angle)
+    y = distance*math.sin(angle)
+    return (x, y)
 
 fileName = "../Data/daily.l7.t1.c006274.20180101.anc-us.warts-DECODED.txt"
+
+sourceNode = -1
 
 G = nx.DiGraph()
 with open(fileName, 'r') as f:
@@ -10,6 +21,7 @@ with open(fileName, 'r') as f:
         splitList = line.split()
         if splitList[0] == 'T':
             sourceIP = splitList[1]
+            sourceNode = sourceIP
             destIP = splitList[2]
             path = splitList[13:]
             for i in range(len(path)):
@@ -26,10 +38,26 @@ with open(fileName, 'r') as f:
 # filtering edges
 G_filtered = nx.DiGraph()
 for edge in G.edges():
-    if G[edge[0]][edge[1]]["weight"] > 8:
+    if G[edge[0]][edge[1]]["weight"] > 40:
         G_filtered.add_edge(edge[0], edge[1], weight=G[edge[0]][edge[1]]["weight"])
 
 print(len(G.edges()))
 print(len(G_filtered.edges()))
 
-nx.write_gml(G_filtered, 'edge-filter-8-weighted-network-from-monitor-'+str(datetime.datetime.today().strftime('%Y-%m-%d'))+".gml")
+pos = {}
+
+for node in G_filtered.nodes():
+    distance = nx.shortest_path_length(G, sourceNode, node)
+    pos[node] = generatePosition(distance)
+
+edges, weights = zip(*nx.get_edge_attributes(G_filtered, 'weight').items())
+
+zipped = zip(weights, edges)
+zipped.sort()
+edges = [edge for (weight, edge) in zipped]
+weights = [weight for (weight, edge) in zipped]
+
+nx.draw(G_filtered, pos, edgelist = edges, node_color='b', node_size=2, width=0.5, edge_color=weights, edge_cmap=plt.cm.autumn, edge_vmin=10)
+plt.savefig('weightes-paths.png', dpi=1000)
+
+#nx.write_gml(G_filtered, 'edge-filter-8-weighted-network-from-monitor-'+str(datetime.datetime.today().strftime('%Y-%m-%d'))+".gml")
