0293b19a606c4c01513c8ff05509e95bacaa8928
EC2 Default User <ec2-user@ip-172-31-89-32.ec2.internal>
2019-09-24 11:16:22 +0000

    As network plots including HyperMap

--- Code/IP2ASmapping.py
+++ Code/IP2ASmapping.py
@@ -10,6 +10,57 @@ import tqdm
 fileName = "../Data/daily.l7.t1.c003736.20150102.ams3-nl-DECODED.txt"
 mappingFile = "../Data/routeviews-rv2-20150102-1200.pfx2as"
 
+def generatePosition(distance):
+    angle = random.uniform(0, 2*math.pi)
+    x = distance*math.cos(angle)
+    y = distance*math.sin(angle)
+    return (x, y)
+
+def positionFromHyperMap():
+    position = {}
+    with open("/home/ec2-user/NetworkPaths/Tools/DkLab/HyperMap-v1/coordinates_embedding.txt", "r") as f:
+        for line in f:
+            splitLine = line.split()
+            angle = float(splitLine[1])
+            radius = float(splitLine[2])
+            position[splitLine[0]] = (math.cos(angle*radius), math.sin(angle*radius))
+    return position
+
+def createGraph(ipDict):
+    
+    global sourceNode
+    sourceNode = -1
+
+    keyCounter = 0
+    nokeyCounter = 0
+
+    G = nx.DiGraph()
+    with open(fileName, 'r') as f:
+        for line in f:
+            splitList = line.split()
+            if splitList[0] == 'T' and splitList[12] == 'C':
+                sourceIP = splitList[1]
+                sourceNode = ipDict[sourceIP]
+                destIP = splitList[2]
+                path = splitList[13:]
+                for i in range(len(path)):
+                    path[i] = path[i].split(",")[0]
+                pathIP = [sourceIP]+path+[destIP]
+                try:
+                    pathAS = [ipDict[i] for i in pathIP]
+                    keyCounter += 1
+                except:
+                    nokeyCounter += 1
+                for i in range(len(pathAS)-1):
+                    source = pathAS[i]
+                    dest = pathAS[i+1]
+                    if not G.has_edge(source, dest):
+                        G.add_edge(source, dest, weight=1)
+                    else:
+                        G[source][dest]["weight"] = G[source][dest]["weight"]+1
+
+    return G
+
 
 def ipToBinary(ipAddr):
     ipList = ipAddr.split('.')
@@ -71,5 +122,44 @@ def createIP2AsMapping():
 #                            G = nx.relabel_nodes(G, {node: asNumber})
 #                            print("Relabel "+str(len(G.nodes())))
 
+def createIP2AsDict():
+    ip2AsDict = {}
+    with open("ip2AsMappings.txt", "r") as f:
+        for line in f:
+            splitLine = line.split()
+            ip2AsDict[splitLine[0]] = splitLine[1]
+    return ip2AsDict
+
 if __name__=="__main__":
-    createIP2AsMapping();
+    # createIP2AsMapping();
+    ip2AsDict = createIP2AsDict()
+    G = createGraph(ip2AsDict)
+    nx.write_edgelist(G, "edgelist.txt")
+
+    pos = {}
+    for node in G.nodes():
+        distance = nx.shortest_path_length(G, sourceNode, node)
+        pos[node] = generatePosition(distance)
+
+    edges, weights = zip(*nx.get_edge_attributes(G, 'weight').items())
+    print(weights)
+    zipped = zip(weights, edges)
+    zipped.sort()
+    edges = [edge for (weight, edge) in zipped]
+    weights = [max(0.1, 0.7*math.log(weight)) for (weight, edge) in zipped]
+
+    #nx.draw(G, pos, edgelist = edges, node_color='b', node_size=5, width=weights, edge_color=weights, edge_cmap=plt.cm.autumn, edge_vmin=1)
+    #plt.savefig('weightes-paths.png', dpi=1000)
+
+    
+    hyperbolicPosition = positionFromHyperMap()
+
+    print(pos)
+    print(hyperbolicPosition)
+
+    nx.draw(G, hyperbolicPosition, edgelist = edges, node_color='b', node_size=5, width=weights, edge_color=weights, edge_cmap=plt.cm.autumn, edge_vmin=1)
+    plt.savefig('hyp-weighted-paths.png', dpi=1000)
+
+
+    print(len(G.nodes()))
+    print(len(G.edges()))
