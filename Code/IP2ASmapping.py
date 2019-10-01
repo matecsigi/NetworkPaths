d3adde3462e15c7f225d9d1f90b30625a9663a79
EC2 Default User <ec2-user@ip-172-31-89-32.ec2.internal>
2019-10-01 09:16:11 +0000

    Add hyperbolic plots for shortest path and difference edge weights

--- Code/IP2ASmapping.py
+++ Code/IP2ASmapping.py
@@ -130,19 +130,53 @@ def createIP2AsDict():
             ip2AsDict[splitLine[0]] = splitLine[1]
     return ip2AsDict
 
+def createShortestPathGraph(G, ipDict):
+    G_shortest = G.copy()
+    for node in G_shortest.nodes():
+        for neighbor in G_shortest.neighbors(node):
+            G_shortest[node][neighbor]["weight"] = 1
+
+    with open(fileName, 'r') as f:
+        for line in f:
+            splitList = line.split()
+            if splitList[0] == 'T' and splitList[12] == 'C':
+                sourceIP = splitList[1]
+                destIP = splitList[2]
+                try:
+                    sourceAs = ipDict[sourceIP]
+                    destAs = ipDict[destIP]
+                    shortestPath = nx.shortest_path(G_shortest, sourceAs, destAs, weight=None)
+                    for i in range(len(shortestPath)-1):
+                        source = shortestPath[i]
+                        dest = shortestPath[i+1]
+                        G_shortest[source][dest]["weight"] = G_shortest[source][dest]["weight"]+1
+                except Exception as e:
+                    print("Error:"+str(e))
+
+    return G_shortest
+
+def createDiffGraph(G, G_shortest):
+    G_diff = G.copy()
+    for node in G.nodes():
+        for neighbor in G.neighbors(node):
+            G_diff[node][neighbor]["weight"] = abs(G[node][neighbor]["weight"]-G_shortest[node][neighbor]["weight"])
+    return G_diff 
+    
+
 if __name__=="__main__":
     # createIP2AsMapping();
     ip2AsDict = createIP2AsDict()
     G = createGraph(ip2AsDict)
     nx.write_edgelist(G, "edgelist.txt")
 
+# Plot original
+#---------------------------------------------------------
     pos = {}
     for node in G.nodes():
         distance = nx.shortest_path_length(G, sourceNode, node)
         pos[node] = generatePosition(distance)
 
     edges, weights = zip(*nx.get_edge_attributes(G, 'weight').items())
-    print(weights)
     zipped = zip(weights, edges)
     zipped.sort()
     edges = [edge for (weight, edge) in zipped]
@@ -151,15 +185,43 @@ if __name__=="__main__":
     #nx.draw(G, pos, edgelist = edges, node_color='b', node_size=5, width=weights, edge_color=weights, edge_cmap=plt.cm.autumn, edge_vmin=1)
     #plt.savefig('weightes-paths.png', dpi=1000)
 
-    
     hyperbolicPosition = positionFromHyperMap()
-
-    print(pos)
-    print(hyperbolicPosition)
 
     nx.draw(G, hyperbolicPosition, edgelist = edges, node_color='b', node_size=5, width=weights, edge_color=weights, edge_cmap=plt.cm.autumn, edge_vmin=1)
     plt.savefig('hyp-weighted-paths.png', dpi=1000)
 
-
     print(len(G.nodes()))
     print(len(G.edges()))
+
+# Plot shortest
+#--------------------------------------------------
+    G_shortest = createShortestPathGraph(G, ip2AsDict)
+    print(len(G_shortest.nodes()))
+    edges, weights = zip(*nx.get_edge_attributes(G_shortest, 'weight').items())
+
+    edges, weights = zip(*nx.get_edge_attributes(G_shortest, 'weight').items())
+    zipped = zip(weights, edges)
+    zipped.sort()
+    edges = [edge for (weight, edge) in zipped]
+    weights = [max(0.1, 0.7*math.log(weight)) for (weight, edge) in zipped]
+
+    hyperbolicPosition = positionFromHyperMap()
+
+    nx.draw(G_shortest, hyperbolicPosition, edgelist = edges, node_color='b', node_size=5, width=weights, edge_color=weights, edge_cmap=plt.cm.autumn, edge_vmin=1)
+    plt.savefig('shortest-hyp-weighted-paths.png', dpi=1000)
+
+# Plot difference
+#--------------------------------------------------
+    G_diff = createDiffGraph(G, G_shortest)
+    print(len(G_diff.nodes()))
+    edges, weights = zip(*nx.get_edge_attributes(G_diff, 'weight').items())
+    print(weights)
+    zipped = zip(weights, edges)
+    zipped.sort()
+    edges = [edge for (weight, edge) in zipped]
+    weights = [max(0.1, 0.7*math.log(max(0.001, weight))) for (weight, edge) in zipped]
+
+    hyperbolicPosition = positionFromHyperMap()
+
+    nx.draw(G_diff, hyperbolicPosition, edgelist = edges, node_color='b', node_size=5, width=weights, edge_color=weights, edge_cmap=plt.cm.autumn, edge_vmin=1, with_labels=True, font_size=2)
+    plt.savefig('diff-hyp-weighted-paths.png', dpi=1000)
